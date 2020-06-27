54c14a0898777e9489eca3956f98cb379fe94827
Umed Jadhav <umed@browserstack.com>
2020-06-27 19:52:49 +0530

    Updated the interpreter to handle whitespaces , multi digit numbers and substraction

--- BasicInterpreter.py
+++ BasicInterpreter.py
@@ -14,29 +14,51 @@ class Interpreter:
     self.text = text
     self.pos = 0 # index into self.text
     self.current_token = None
+    self.current_char = self.text[self.pos]
 
   def error(self):
     raise Exception('Error parsing input')
 
+  def advance(self):
+    self.pos += 1
+    if self.pos > len(self.text) - 1:
+      self.current_char = None
+    else:
+      self.current_char = self.text[self.pos]
+  
+  def skip_whitespace(self):
+    while self.current_char is not None and self.current_char.isspace():
+      self.advance()
+  
+  def integer(self):
+    result = ''
+    while self.current_char is not None and self.current_char.isdigit():
+      result += self.current_char
+      self.advance()
+    return int(result)
 
   def get_next_token(self):
-    text = self.text
-    if self.pos > len(text) - 1 :
-      return Token('EOF', None)
-    
-    curr_char = text[self.pos]
+    # Lexical Analyzer
+    while self.current_char is not None:
+      
+      if self.current_char.isspace():
+        self.skip_whitespace()
+        continue
 
-    if curr_char.isdigit():
-      token = Token('INTEGER',int(curr_char))
-      self.pos += 1
-      return token
+      if self.current_char.isdigit():
+        return Token('INTEGER',self.integer())
+
+      if self.current_char ==  '+':
+        self.advance()
+        return  Token('PLUS','+')
+
+      if self.current_char ==  '-':
+        self.advance()
+        return  Token('MINUS','-')      
     
-    if curr_char ==  '+':
-      token = Token('PLUS',curr_char)
-      self.pos += 1
-      return token
-    
-    self.error() # If the token is not any of the recognized one , raise an error
+      self.error() # If the token is not any of the recognized one , raise an error
+
+    return Token('EOF',None)
 
   def eat(self, token_type):
     if self.current_token._type == token_type :
@@ -50,12 +72,18 @@ class Interpreter:
     self.eat('INTEGER')
 
     op = self.current_token
-    self.eat('PLUS')
+    if op._type == 'PLUS':
+      self.eat('PLUS')
+    else:
+      self.eat('MINUS')
 
     right = self.current_token
     self.eat('INTEGER')
 
-    result = left.value + right.value
+    if op._type == 'PLUS':
+      result = left.value + right.value
+    else:
+      result = left.value - right.value
     return result
 
 if __name__ == "__main__":
