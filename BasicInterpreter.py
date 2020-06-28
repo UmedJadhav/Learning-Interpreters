57d73135e791ab3ca6cc068ccb91442fafc34877
Umed Jadhav <umed@browserstack.com>
2020-06-28 10:04:57 +0530

    Added  the ability of understanding precedance rule - Mulitplicative operations should be computed before additive operations

--- BasicInterpreter.py
+++ BasicInterpreter.py
@@ -9,11 +9,10 @@ class Token:
   def __repr__(self):
     return self.__str__()
 
-class Interpreter:
+class Lexer:
   def __init__(self, text):
     self.text = text
     self.pos = 0 # index into self.text
-    self.current_token = None
     self.current_char = self.text[self.pos]
 
   def error(self):
@@ -25,11 +24,11 @@ class Interpreter:
       self.current_char = None
     else:
       self.current_char = self.text[self.pos]
-  
+
   def skip_whitespace(self):
     while self.current_char is not None and self.current_char.isspace():
       self.advance()
-  
+
   def integer(self):
     # Returns a multidigit integer consumed from input
     result = ''
@@ -69,23 +68,47 @@ class Interpreter:
 
     return Token('EOF',None)
 
+class Interpreter:
+  def __init__(self, lexer):
+    self.lexer = lexer
+    self.current_token = self.lexer.get_next_token()
+  
+  def error(self):
+    raise Exception('Invalid Syntax')
+
   def eat(self, token_type):
     if self.current_token._type == token_type :
-      self.current_token = self.get_next_token()
+      self.current_token = self.lexer.get_next_token()
     else:
       self.error()
 
-  def term(self):
-    # Returns an INTEGER token value
+  def factor(self):
+    # factor : INTEGER '
     token = self.current_token
     self.eat('INTEGER')
     return token.value
+  
+  def term(self):
+    # handles term part of grammar term: factor( (MUL | DIV ) factor)
+    result = self.factor()
+    while self.current_token._type in ('MUL', 'DIV'):
+      token = self.current_token
+      if token._type == 'MUL':
+        self.eat('MUL')
+        result = result * self.factor()
+      elif token._type == 'DIV':
+        self.eat('DIV')
+        result = result / self.factor()
+    return result 
 
   def expr(self):
     # Arithmetic expression parser / interpreter
-    self.current_token = self.get_next_token()
+    # Parser Grammar with the precedence rule 
+    # expr : term((PLUS | MINUS )term )*  * -> any number of arguments 
+    # term : factor ((MUL | DIV ) factor)*
+    # factor : INTEGER
     result = self.term()
-    while self.current_token._type in ('PLUS','MINUS','MUL','DIV'):
+    while self.current_token._type in ('PLUS','MINUS'):
       token = self.current_token
       if token._type == 'PLUS':
         self.eat('PLUS')
@@ -93,13 +116,6 @@ class Interpreter:
       elif token._type == 'MINUS':
         self.eat('MINUS')
         result = result -  self.term()
-      elif token._type == 'MUL':
-        self.eat('MUL')
-        result = result *  self.term()
-      elif token._type == 'DIV':
-        self.eat('DIV')
-        result = result /  self.term()
-  
     return result
 
 
@@ -111,6 +127,7 @@ if __name__ == "__main__":
       break
     if not text:
       continue
-    interpreter = Interpreter(text)
+    lexer = Lexer(text)
+    interpreter = Interpreter(lexer)
     result = interpreter.expr()
     print(result)
