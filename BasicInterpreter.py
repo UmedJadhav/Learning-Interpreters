6562d0eace7444654876282f7710b615d065c0c2
Umed Jadhav <umed@browserstack.com>
2020-07-07 09:30:24 +0530

    Added new visitor methods in Interpreter and introduced the concept of global scope

--- BasicInterpreter.py
+++ BasicInterpreter.py
@@ -342,6 +342,26 @@ class Interpreter(NodeVisitor):
       return +self.visit(node.expr)
     elif op == 'MINUS':
       return -self.visit(node.expr)
+  
+  def visit_Compound(self, node):
+    for child in node.children:
+      self.visit(child)
+  
+  def visit_NoOp(self, node):
+    pass
+
+  def visit_Assign(self, node):
+    # GLOBAL_SCOPE is a symbol table cum Memory space
+    var_name = self.left.value
+    self.GLOBAL_SCOPE[var_name] = self.visit(node.right)
+  
+  def visit_Var(self, node):
+    var_name = node.value
+    val = self.GLOBAL_SCOPE.get(var_name)
+    if val is None:
+      raise NameError(repr(var_name))
+    else:
+      return val
 
   def visit_Num(self, node):
     return node.value
